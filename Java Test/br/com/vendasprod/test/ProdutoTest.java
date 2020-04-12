c40d7a6b6d07e250c04e8a5f9bb8cc7b1afa1760
weydson <weydson.macedo@gmail.com>
2020-04-12 20:12:48 -0300

    melhorando interface

---
+++ Java Test/br/com/vendasprod/test/ProdutoTest.java
@@ -0,0 +1,17 @@
+//package br.com.vendasprod.test;
+//
+//import javax.persistence.EntityManagerFactory;
+//import javax.transaction.Transaction;
+//
+//import org.hibernate.Session;
+//
+//class ProdutoTest {
+//
+//	private Session sessao;
+//	private Transaction transacao;
+//	
+//	public void openconection() {
+//		this.sessao = EntityManagerFactory
+//	}
+//
+//}
